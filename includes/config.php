4e1d4243b786fc90585b505b401a776fe5878770
Pierre-Luc <pierre.luc.bertrand@phonop.com>
2017-09-12 12:31:51 -0400

    first commit

---
+++ includes/config.php
@@ -0,0 +1,8 @@
+<?php
+
+define("CONFIG_DEFAULT_FLAVOR","default");
+define("CONFIG_MENU_WIDTH","default");
+define("CONFIG_SHOW_ONLINE_USERS", false);
+define("CONFIG_SHOW_CHAT", false);
+define("CONFIG_SHOW_PROFILE", false);
+define("CONFIG_SHOW_NOTIFICATIONS", true);
